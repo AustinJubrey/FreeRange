5bc166fef61e5bcfd29ab475a807fb2f26b1b2ec
Austin <austinjubrey@gmail.com>
2022-10-10 16:50:34 -0400

    [Tech] [Gameplay] Jumping
    Players can now jump using the spacebar, though currently it can be spammed.

--- MultiplayerAnimalTest/Assets/FishNet/Example/All/Prediction/CharacterController/Scripts/CharacterControllerPrediction.cs
+++ MultiplayerAnimalTest/Assets/FishNet/Example/All/Prediction/CharacterController/Scripts/CharacterControllerPrediction.cs
@@ -20,15 +20,18 @@ namespace FishNet.Example.Prediction.CharacterControllers
             public float Horizontal;
             public float Vertical;
             public float MouseHorizontal;
+            public bool Jump;
         }
         public struct ReconcileData
         {
             public Vector3 Position;
             public Quaternion Rotation;
-            public ReconcileData(Vector3 position, Quaternion rotation)
+            public float VerticalVelocity;
+            public ReconcileData(Vector3 position, Quaternion rotation, float verticalVelocity)
             {
                 Position = position;
                 Rotation = rotation;
+                VerticalVelocity = verticalVelocity;
             }
         }
         #endregion
@@ -41,6 +44,10 @@ namespace FishNet.Example.Prediction.CharacterControllers
         #region Private.
         private CharacterController _characterController;
         private Vector3 _rotationSpeed = new Vector3(0,270,0);
+        private float _ySpeed = 0;
+        private float _jumpHeight = 0.5f;
+        private float _gravity = -12f;
+        private float _terminalVelocity = -10f;
         private bool _canMove = true;
         #endregion
 
@@ -76,6 +83,8 @@ namespace FishNet.Example.Prediction.CharacterControllers
             if (IsServer)
             {
                 Move(default, true);
+                ReconcileData rd = new ReconcileData(transform.position, transform.rotation, _ySpeed);
+                Reconciliation(rd, true);
             }
         }
 
@@ -84,7 +93,7 @@ namespace FishNet.Example.Prediction.CharacterControllers
             if (transform == null)
                 return;
             
-            ReconcileData rd = new ReconcileData(transform.position, transform.rotation);
+            ReconcileData rd = new ReconcileData(transform.position, transform.rotation, _ySpeed);
             Reconciliation(rd, true);
         }
 
@@ -92,19 +101,20 @@ namespace FishNet.Example.Prediction.CharacterControllers
         {
             md = default;
 
-            float horizontal = Input.GetAxisRaw("Horizontal");
-            float vertical = Input.GetAxisRaw("Vertical");
-
+            float horizontal = _canMove ? Input.GetAxisRaw("Horizontal") : 0f;
+            float vertical = _canMove ? Input.GetAxisRaw("Vertical") : 0f;
             float mouseHorizontal = Input.GetAxisRaw("Mouse X");
+            bool jump = _canMove && Input.GetKeyDown(KeyCode.Space);
 
-            if (horizontal == 0f && vertical == 0f && mouseHorizontal == 0f)
+            if (horizontal == 0f && vertical == 0f && mouseHorizontal == 0f && !jump)
                 return;
 
             md = new MoveData()
             {
                 Horizontal = horizontal,
                 Vertical = vertical,
-                MouseHorizontal = mouseHorizontal
+                MouseHorizontal = mouseHorizontal,
+                Jump = jump
             };
         }
 
@@ -117,11 +127,17 @@ namespace FishNet.Example.Prediction.CharacterControllers
                 transform.Rotate(rotation);
             }
 
-            if (_canMove && (md.Horizontal != 0f || md.Vertical != 0f))
+            if (md.Jump)
             {
-                Vector3 move = new Vector3(md.Horizontal, 0f, md.Vertical).normalized + new Vector3(0f, Physics.gravity.y, 0f);
-                _characterController.Move(transform.rotation * move * _moveRate * (float)TimeManager.TickDelta);
+                _ySpeed = Mathf.Sqrt(_jumpHeight * -2f * _gravity);
             }
+            
+            if(_ySpeed > _terminalVelocity)
+                _ySpeed += _gravity * (float)TimeManager.TickDelta;
+            
+            Vector3 movementDirection = new Vector3(md.Horizontal, 0f, md.Vertical).normalized;
+
+            _characterController.Move((transform.rotation * movementDirection * _moveRate * (float)TimeManager.TickDelta) + new Vector3(0f, _ySpeed, 0f) * (float)TimeManager.TickDelta);
         }
 
         [Reconcile]
@@ -129,6 +145,7 @@ namespace FishNet.Example.Prediction.CharacterControllers
         {
             transform.position = rd.Position;
             transform.rotation = rd.Rotation;
+            _ySpeed = rd.VerticalVelocity;
         }
 
         [TargetRpc]
